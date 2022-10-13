9ee4fc845402a3f215d9bb885940769c5c3df3b6
dummy <you@dummy.com>
2022-10-13 04:51:15 +0000

    enable start cmd with pm2

--- sync/admin/start.sh
+++ sync/admin/start.sh
@@ -14,10 +14,11 @@ export TS=$(date +%Y%m%d%H%M%S)
 
 # main 
 [ -z "${BASH_SOURCE[0]}" -o "${BASH_SOURCE[0]}" = "$0" ] || return
+echo "First install pm2 by `npm install -g pm2`"
 cd $baseDir/../app
 source .env
 #export $(grep -v '^#' .env | xargs -d '\n')
 echo $DEBUG
 
 set -x
-npm start
+pm2 start app.js -n cskefu.sync
